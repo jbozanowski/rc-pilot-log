1f2034aedda50638c333d4b65756a7df752b3625
Jakub Bożanowski <jbozanowski@gmail.com>
2013-04-28 18:37:08 +0200

    Filling in a basic Event model.

--- rcpilotlog/events/models.py
+++ rcpilotlog/events/models.py
@@ -9,6 +9,9 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from rcpilotlog.batteries.models import Battery
+from rcpilotlog.rcmodels.models import RCModel
+
 
 log = logging.getLogger(__name__)
 
@@ -24,7 +27,23 @@ class EventType(TimeStampedModel):
 
 
 class Event(TimeStampedModel):
-    pass
+    event_type = models.ForeignKey(EventType, verbose_name=_(u"event type"))
+    description = models.TextField(_(u"name"), blank=True)
+    user = models.ForeignKey(User, verbose_name=_(u"user"))
+    rcmodel = models.ForeignKey(RCModel, verbose_name=_(u"RC model"), null=True)
+    battery = models.ForeignKey(Battery, verbose_name=_(u"battery"), null=True)
+    duration = models.IntegerField(_(u"duration"), null=True)
+    description = models.TextField(_(u"description"))
 
     class Meta:
-        pass
+        verbose_name = _(u"Event")
+        verbose_name_plural = _(u"Events")
+        ordering = ("user", "-created")
+
+    def __unicode__(self):
+        # FIXME
+        return u"%s (%s @ %s)" % (
+            self.get_event_type_display(),
+            self.user,
+            self.created
+        )
