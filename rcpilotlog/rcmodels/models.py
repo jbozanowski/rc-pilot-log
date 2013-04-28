98d057cd5fdbe25db98f6ae3231fe62ccc8ee563
Jakub Bożanowski <jbozanowski@gmail.com>
2013-04-28 19:49:00 +0200

    Changing RCModel's type field.
    Denormalizing from a relation to a choice. Makes more sense for translations,
    and doesn't really need the flexibility of a separate model.

--- rcpilotlog/rcmodels/models.py
+++ rcpilotlog/rcmodels/models.py
@@ -2,6 +2,7 @@
 
 import logging
 
+from model_utils import Choices
 from model_utils.models import TimeStampedModel
 
 from django.db import models
@@ -28,9 +29,18 @@ class RCModelType(TimeStampedModel):
 
 
 class RCModel(TimeStampedModel):
+    MODEL_TYPES = Choices(
+        ('heli', _(u"Helicopter")),
+        ('acro', _(u"Airplane")),
+        ('glider', _(u"Glider")),
+        ('motoglider', _(u"Motor Glider")),
+        ('multicopter', _(u"Multicopter")),
+    )
+
     name = models.CharField(_(u"name"), max_length=255)
-    rcmodel_type = models.ForeignKey(RCModelType,
-        verbose_name=_(u"Your RC Model's Type"))
+    rcmodel_type = models.CharField(_(u"Your RC Model's Type"),
+                                    choices=MODEL_TYPES,
+                                    default=MODEL_TYPES.heli, max_length=64)
     owner = models.ForeignKey(User, verbose_name=_(u"Owner"))
     manufacturer = models.CharField(_(u"manufacturer"), max_length=255)
     description = models.TextField(_(u"description"), blank=True)
