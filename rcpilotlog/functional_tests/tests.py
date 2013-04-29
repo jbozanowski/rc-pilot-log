bf8ddede8118cd96fc546a6843c2ddc8653c70af
Jakub Bożanowski <jbozanowski@gmail.com>
2013-04-29 20:29:35 +0200

    Adding object creation tests.

--- rcpilotlog/functional_tests/tests.py
+++ rcpilotlog/functional_tests/tests.py
@@ -6,13 +6,43 @@ from django.contrib.auth.models import User
 from django.test import LiveServerTestCase
 from django.test.client import Client
 
+from rcpilotlog.batteries.models import Battery
+from rcpilotlog.events.models import Event
+from rcpilotlog.rcmodels.models import RCModel
+
 
 log = logging.getLogger(__name__)
 
 test_admin_user = {
+    'username': 'admin_tester',
+    'email': 'admin_tester@test.com',
+    'password': 'test'
+}
+
+test_user = {
     'username': 'tester',
     'email': 'tester@test.com',
     'password': 'test'
+}
+
+test_battery = {
+    'name': 'Test Battery',
+    # 'owner' needs a user created first,
+    'manufacturer': 'Test Battery Manufacturer',
+    'capacity': 2200,
+    'chemistry': Battery.CHEMISTRY.lipo,
+}
+
+test_rcmodel = {
+    'name': 'Test RC Model',
+    'rcmodel_type': RCModel.MODEL_TYPES.heli,
+    # 'owner' needs a user created first,
+    'manufacturer': 'Test RC Model Manufacturer',
+}
+
+test_event = {
+    'event_type': Event.EVENT_TYPES.flight,
+    # 'user' needs a user created first,
 }
 
 
@@ -20,13 +50,13 @@ class RCPilotLogTest(LiveServerTestCase):
     """Site-wide functional tests."""
 
     def setUp(self):
-        User.objects.create_superuser(**test_admin_user)
+        self.user = User.objects.create_user(**test_user)
+        self.admin_user = User.objects.create_superuser(**test_admin_user)
         self.c = Client()
 
     def tearDown(self):
-        u = User.objects.get(username=test_admin_user['username'],
-                             email=test_admin_user['email'])
-        u.delete()
+        self.user.delete()
+        self.admin_user.delete()
 
     def test_basic_gets(self):
         resp = self.c.get('/admin/')
@@ -37,3 +67,14 @@ class RCPilotLogTest(LiveServerTestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertIn("body", resp.content)
         self.assertIn("<title>", resp.content)
+
+    def test_creating_objects(self):
+        """Make sure we can actually create all of application's models."""
+
+        test_battery.update({'owner': self.user})
+        test_rcmodel.update({'owner': self.user})
+        test_event.update({'user': self.user})
+
+        battery = Battery.objects.create(**test_battery)
+        rcmodel = RCModel.objects.create(**test_rcmodel)
+        event = Event.objects.create(**test_event)
